6f3b99f9d5e9084d2f85fd03b4e71b9061dd11ac
jc320680 <tariq.tarbuck@my.jcu.edu.au>
2016-08-15 11:31:03 +1000

    Adding code which was part of the Prac02

--- oddName.py
+++ oddName.py
@@ -1,1 +1,6 @@
 '''Tariq Tarbuck'''
+name = input(int("what is your name"))
+while name != " ":
+    name = input(int("what is your name"))
+    for i in range (1,len(name),2):
+        print(i,end"" )
